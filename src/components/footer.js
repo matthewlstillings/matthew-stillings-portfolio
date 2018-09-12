83267a2cae88449d96d9fe4f1a751e90fbf49ed2
Matthew Stillings <matthewstillings@Matthews-MacBook-Pro.local>
2018-09-11 22:59:04 -0400

    fixed nav hover animation

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,8 @@ export const Footer = () => {
             <div className="social-media__links-container is-expanded">
                 <a className="social-media-bar__contact__link is-email is-expanded"  href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=matthewlstillings@gmail.com" target="_blank"><p className="link-text is-expanded">Email</p></a>
                 <a href="https://twitter.com/AngstGehirn" className="social-media-bar__contact__link is-twitter is-expanded"><p className="link-text is-expanded">Twitter</p></a>
-                <a href="https://www.linkedin.com/in/matthew-stillings-4a2582141/" className="social-media-bar__contact__link is-linkedin is-expanded"><p className="link-text is-expanded">LinkedIn</p></a>
                 <a href="https://github.com/matthewlstillings" className="social-media-bar__contact__link is-github is-expanded"><p className="link-text is-expanded">Github</p></a>
+                <a href="https://www.linkedin.com/in/matthew-stillings-4a2582141/" className="social-media-bar__contact__link is-linkedin is-expanded"><p className="link-text is-expanded">LinkedIn</p></a>
             </div>
         </footer>
     )
--- src/components/social-media.js
+++ src/components/social-media.js
@@ -23,8 +23,8 @@ export const SocialMedia = (props) => (
         >
             <a className={"social-media-bar__contact__link is-email " + (props.linkDisplay == true && " is-expanded" )} href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=matthewlstillings@gmail.com" target="_blank"><p className={"link-text " + (props.linkDisplay == true && " is-expanded" )}>Email</p></a>
             <a href="https://twitter.com/AngstGehirn" className={"social-media-bar__contact__link is-twitter " + (props.linkDisplay == true && " is-expanded" )}><p className={"link-text " + (props.linkDisplay == true && " is-expanded" )}>Twitter</p></a>
-            <a href="https://www.linkedin.com/in/matthew-stillings-4a2582141/" className={"social-media-bar__contact__link is-linkedin "  + (props.linkDisplay == true && " is-expanded" )}><p className={"link-text " + (props.linkDisplay == true && " is-expanded" )}>LinkedIn</p></a>
             <a href="https://github.com/matthewlstillings" className={"social-media-bar__contact__link is-github " + (props.linkDisplay == true && " is-expanded" )}><p className={"link-text " + (props.linkDisplay == true && " is-expanded" )}>Github</p></a>
+            <a href="https://www.linkedin.com/in/matthew-stillings-4a2582141/" className={"social-media-bar__contact__link is-linkedin "  + (props.linkDisplay == true && " is-expanded" )}><p className={"link-text " + (props.linkDisplay == true && " is-expanded" )}>LinkedIn</p></a>
         </div>
     </div> 
 )
